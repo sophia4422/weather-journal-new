5815de0c3a2dfba5a93b2179d3045e699e67f25c
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-05-30 14:10:32 +0100

    render cards from js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const recentSearchesContainer = $("#recent-searches-container");
+const weatherInfoContainer = $("#weather-info-container");
 const searchForm = $("#search-form");
 
 const readFromLocalStorage = (key, defaultValue) => {
@@ -21,6 +22,198 @@ const writeToLocalStorage = (key, value) => {
 
   //set stringified value to LS for keyname
   localStorage.setItem(key, stringifiedValue);
+};
+
+const renderCurrentData = () => {
+  const currentWeatherCard = `<div class="col-sm-12 col-md-9" id="weather-info-container">
+  <!-- current data -->
+  <div class="text-center">
+    <div class="current-weather-card text-white p-3">
+    <h2>Orlando</h2>
+    <h3>Sat 28th May 2022</h3>
+    <div><img src="http://openweathermap.org/img/w/04d.png" alt="icon of clouds"/></div>
+  </div>
+
+    <!-- weather metrics div -->
+    <div>
+      <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-2 border fw-bold">Temperature</div>
+        <div class="col-sm-12 col-md-8 p-2 border">20&deg;C</div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-2 border fw-bold">Humidity</div>
+        <div class="col-sm-12 col-md-8 p-2 border">20&percnt;</div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-2 border fw-bold">Windspeed</div>
+        <div class="col-sm-12 col-md-8 p-2 border">10 mpH</div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-sm-12 col-md-4 p-2 border fw-bold">UV Index</div>
+        <div class="col-sm-12 col-md-8 p-2 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+      </div>
+  </div>
+</div>`;
+
+  weatherInfoContainer.append(currentWeatherCard);
+};
+
+const renderForecastData = () => {
+  const forecastWeatherCards = `<div>
+        <h2 class="mt-3 text-center">5 day Forecast</h2>
+        <div class="d-flex flex-row justify-content-center flex-wrap">
+        <!-- card one -->
+        <div class="card m-2 forecast-card">
+            <div class="d-flex justify-content-center">
+        <img src="http://openweathermap.org/img/w/04d.png" class="card-img-top weather-icon" alt="weather icon">
+        <div class="card-body">
+            <h5 class="card-title text-center">Sun 22nd May</h5>
+            <div class="mt-4 text-center">
+            <div class="row g-0">
+                <div class="col-12 border bg-light fw-bold">Temperature</div>
+                <div class="col-12 border">20&deg;C</div>
+            </div>
+            <div class="row g-0">
+                <div class="col-12 border bg-light fw-bold">Humidity</div>
+                <div class="col-12 border">20&percnt;</div>
+            </div>
+
+            <div class="row g-0">
+                <div class="col-12 border bg-light fw-bold">Windspeed</div>
+                <div class="col-12 border">10 mpH</div>
+            </div>
+
+            <div class="row g-0">
+                <div class="col-12 border bg-light fw-bold">UV Index</div>
+                <div class="col-12 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    <!-- card two -->
+    <div class="card m-2 forecast-card">
+        <div class="d-flex justify-content-center">
+    <img src="http://openweathermap.org/img/w/04d.png" class="card-img-top weather-icon" alt="weather icon">
+    <div class="card-body">
+        <h5 class="card-title text-center">Sun 22nd May</h5>
+        <div class="mt-4 text-center">
+        <div class="row g-0">
+            <div class="col-12 border bg-light fw-bold">Temperature</div>
+            <div class="col-12 border">20&deg;C</div>
+        </div>
+        <div class="row g-0">
+            <div class="col-12 border bg-light fw-bold">Humidity</div>
+            <div class="col-12 border">20&percnt;</div>
+        </div>
+
+        <div class="row g-0">
+            <div class="col-12 border bg-light fw-bold">Windspeed</div>
+            <div class="col-12 border">10 mpH</div>
+        </div>
+
+        <div class="row g-0">
+            <div class="col-12 border bg-light fw-bold">UV Index</div>
+            <div class="col-12 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+        </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    <!-- card three -->
+    <div class="card m-2 forecast-card">
+    <div class="d-flex justify-content-center">
+    <img src="http://openweathermap.org/img/w/04d.png" class="card-img-top weather-icon" alt="weather icon">
+    <div class="card-body">
+    <h5 class="card-title text-center">Sun 22nd May</h5>
+    <div class="mt-4 text-center">
+    <div class="row g-0">
+        <div class="col-12 border bg-light fw-bold">Temperature</div>
+        <div class="col-12 border">20&deg;C</div>
+    </div>
+    <div class="row g-0">
+        <div class="col-12 border bg-light fw-bold">Humidity</div>
+        <div class="col-12 border">20&percnt;</div>
+    </div>
+
+    <div class="row g-0">
+        <div class="col-12 border bg-light fw-bold">Windspeed</div>
+        <div class="col-12 border">10 mpH</div>
+    </div>
+
+    <div class="row g-0">
+        <div class="col-12 border bg-light fw-bold">UV Index</div>
+        <div class="col-12 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <!-- card four -->
+    <div class="card m-2 forecast-card">
+    <div class="d-flex justify-content-center">
+    <img src="http://openweathermap.org/img/w/04d.png" class="card-img-top weather-icon" alt="weather icon">
+    <div class="card-body">
+    <h5 class="card-title text-center">Sun 22nd May</h5>
+    <div class="mt-4 text-center">
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Temperature</div>
+    <div class="col-12 border">20&deg;C</div>
+    </div>
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Humidity</div>
+    <div class="col-12 border">20&percnt;</div>
+    </div>
+
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Windspeed</div>
+    <div class="col-12 border">10 mpH</div>
+    </div>
+
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">UV Index</div>
+    <div class="col-12 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+    </div>
+    </div>
+
+    </div>
+    </div>
+    </div>
+    <!-- card five -->
+    <div class="card m-2 forecast-card">
+    <div class="d-flex justify-content-center">
+    <img src="http://openweathermap.org/img/w/04d.png" class="card-img-top weather-icon" alt="weather icon">
+    <div class="card-body">
+    <h5 class="card-title text-center">Sun 22nd May</h5>
+    <div class="mt-4 text-center">
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Temperature</div>
+    <div class="col-12 border">20&deg;C</div>
+    </div>
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Humidity</div>
+    <div class="col-12 border">20&percnt;</div>
+    </div>
+
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">Windspeed</div>
+    <div class="col-12 border">10 mpH</div>
+    </div>
+
+    <div class="row g-0">
+    <div class="col-12 border bg-light fw-bold">UV Index</div>
+    <div class="col-12 border"><span class="bg-success text-white px-3 rounded-2">1.5</span></div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>`;
+
+  weatherInfoContainer.append(forecastWeatherCards);
 };
 
 const renderRecentSearches = () => {
@@ -76,6 +269,13 @@ const handleFormSubmit = (event) => {
   const cityName = $("#search-input").val();
 
   if (cityName) {
+    //fetch data from API
+
+    // render current data
+    renderCurrentData();
+    //render forecast data
+    renderForecastData();
+
     //get recent searches from LS
     const recentSearches = readFromLocalStorage("recentSearches", []);
 
